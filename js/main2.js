cff21df474c9005966c7699b07037e2bb4032445
JudyDukmak <joudydokkmak@gmail.com>
2023-08-14 21:10:51 +0300

    array and objects in js

---
+++ js/main2.js
@@ -0,0 +1,22 @@
+let arr= [5,2, 1,4,8]; 
+let divs= document.getElementsByClassName("t1"); 
+ 
+/*document.getElementById("container").innerHTML = arr ; 
+console.log (arr); 
+ 
+let s =""; 
+for ( let i=0; i<arr.length ; i++) 
+{ s  += arr[i] + "<br>"; 
+} 
+document.getElementById("container").innerHTML = s; 
+ 
+divs[0] .innerHTML = "judy"; 
+divs[1] .innerHTML = "sana"; 
+ 
+console.log(divs); 
+*/ 
+ 
+let min = arr[0]; 
+ 
+for (let i=0 ;i<arr.length; i++) 
+{  divs[i]=arr[i];}
